f4bd2551f0691ef96db16eef7ab4ade0a05ba654
user <user@DESKTOP-05J4HLG>
2016-12-09 14:45:30 +0200

    go...

--- Viewer/MainWindow.xaml.cs
+++ Viewer/MainWindow.xaml.cs
@@ -12,6 +12,7 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using System.IO;
 
 namespace Viewer
 {
@@ -20,15 +21,76 @@ namespace Viewer
     /// </summary>
     public partial class MainWindow : Window
     {
+        List<Image> allPictures = new List<Image>();
+        //List<Image> allControls = new List<Image>();
         public MainWindow()
         {
             InitializeComponent();
+            myInitialize();
+
+
             this.AllowDrop = true;
             MainGrid.AllowDrop = true;
             MainGrid.Drop += MainGrid_Drop;
             this.Drop += MainGrid_Drop;
         }
 
+        private void myInitialize()
+        {
+            //изменить фильтр/паттерн поиска
+            List<string> allNamePictures = Directory.GetFiles(System.Environment.CurrentDirectory, "*.png").ToList();
+            foreach (string imgPath in allNamePictures)
+            {
+                Image NewImage = new Image();
+                BitmapImage bitImg = new BitmapImage();
+                bitImg.BeginInit();
+                bitImg.UriSource = new Uri(imgPath);
+                bitImg.EndInit();
+                NewImage.Stretch = Stretch.Fill;
+                NewImage.Source = bitImg;
+                NewImage.Height = 150;
+                allPictures.Add(NewImage);
+            }
+
+            foreach (Image img in allPictures)
+            {
+                MainGrid.
+            }
+            //добавление изначально имеющихся контролов, остальные будут добавлятся по необходимости
+            //allControls.Add(image1_1);
+            //allControls.Add(image1_2);
+            //allControls.Add(image1_3);
+            //allControls.Add(image1_4);
+            
+            //заполняем Image контролы, картинками из коллекции.
+            //foreach (Image img in allPictures)
+            //{
+            //    //if(image1_1.Source == null)
+            //    //{
+            //    //    image1_1.Source = img.Source;
+            //    //    continue;
+            //    //}
+            //    //if (image1_2.Source == null)
+            //    //{
+            //    //    image1_2.Source = img.Source;
+            //    //    continue;
+            //    //}
+            //    //if (image1_3.Source == null)
+            //    //{
+            //    //    image1_3.Source = img.Source;
+            //    //    continue;
+            //    //}
+            //    //if (image1_4.Source == null)
+            //    //{
+            //    //    image1_4.Source = img.Source;
+            //    //    continue;
+            //    //}
+            //    //else
+            //    //{
+            //        allControls.Add(img);
+            //    //}
+            //}
+        }
         private void MainGrid_Drop(object sender, DragEventArgs e)
         {
             textBox.Text = sender.GetType().ToString() + "\n";
@@ -37,18 +99,31 @@ namespace Viewer
                 textBox.Text += item + "\n";
             }
             //путь картинки
-            Image myImage = new Image();
-            BitmapImage bi3 = new BitmapImage();
-            bi3.BeginInit();
-            bi3.UriSource = new Uri(@"C:\Users\user\Documents\visual studio 2015\Projects\Viewer\Viewer\bin\Debug\1.png");
-            bi3.EndInit();
-            myImage.Stretch = Stretch.Fill;
-            myImage.Source = bi3;
-            image1_1.Source = myImage.Source;
             
+           // image1_1.Source = myImage.Source;
+
             //MainGrid.Background = Brushes.Gold;
             //this.Background = Brushes.ForestGreen;
             //this.Title = "Drop";
+
+            // Загрузить все файлы *.jpg и создать новую папку для модифицированных данных,
+            //string[] files = Directory.GetFiles(@"C:\Users\Public\Pictures\Sample Pictures", "*.jpg", SearchOption.AllDirectories);
+            //string newDir = @"C:\ModifiedPictures";
+            //Directory.CreateDirectory(newDir);
+            //// Обработать данные изображений в блокирующей манере.
+            //foreach (string currentFile in files)
+            //{
+            //    string filename = System.IO.Path.GetFileName(currentFile);
+            //    using (Bitmap bitmap = new Bitmap(currentFile))
+            //    {
+            //        bitmap.RotateFlip(RotateFlipType.Rotatel80FlipNone);
+            //        bitmap.Save(Path.Combine(newDir, filename));
+            //        // Вывести идентификатор потока, обрабатывающего текущее изображение.
+            //        this.Text = string.Format("Processing {0} on thread {1}", filename,
+            //        Thread.CurrentThread.ManagedThreadId);
+            //    }
+            //}
+
         }
     }
 }
