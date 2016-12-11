13f8e2461b5f7fa74031f242e15bfa1eaa36ee2d
user <user@DESKTOP-05J4HLG>
2016-12-11 13:22:39 +0200

    перетягивание работает!

--- Viewer/MainWindow.xaml.cs
+++ Viewer/MainWindow.xaml.cs
@@ -25,7 +25,7 @@ namespace Viewer
         public MainWindow()
         {
             InitializeComponent();
-           // myInitialize();
+            // myInitialize();
 
             MainGrid.AllowDrop = true;
             MainGrid.Drop += MainGrid_Drop;
@@ -34,13 +34,13 @@ namespace Viewer
         private void myInitialize()
         {
             //изменить фильтр/паттерн поиска
-           List<string> listBmp = Directory.GetFiles(System.Environment.CurrentDirectory, "*.bmp").ToList();
-           List<string> listJpeg = Directory.GetFiles(System.Environment.CurrentDirectory, "*.jpeg").ToList();
-           List<string> listJpg = Directory.GetFiles(System.Environment.CurrentDirectory, "*.jpg").ToList();
+            List<string> listBmp = Directory.GetFiles(System.Environment.CurrentDirectory, "*.bmp").ToList();
+            List<string> listJpeg = Directory.GetFiles(System.Environment.CurrentDirectory, "*.jpeg").ToList();
+            List<string> listJpg = Directory.GetFiles(System.Environment.CurrentDirectory, "*.jpg").ToList();
             List<string> listPng = Directory.GetFiles(System.Environment.CurrentDirectory, "*.png").ToList();
-           List<string> listIco = Directory.GetFiles(System.Environment.CurrentDirectory, "*.ico").ToList();
-           List<string> listGif = Directory.GetFiles(System.Environment.CurrentDirectory, "*.gif").ToList();
-           List<string> listTiff = Directory.GetFiles(System.Environment.CurrentDirectory, "*.tiff").ToList();
+            List<string> listIco = Directory.GetFiles(System.Environment.CurrentDirectory, "*.ico").ToList();
+            List<string> listGif = Directory.GetFiles(System.Environment.CurrentDirectory, "*.gif").ToList();
+            List<string> listTiff = Directory.GetFiles(System.Environment.CurrentDirectory, "*.tiff").ToList();
             IEnumerable<string> allNamePictures = (((((listBmp.Concat(listJpeg)).Concat(listJpg)).Concat(listPng)).Concat(listIco)).Concat(listGif)).Concat(listTiff);
             foreach (string imgPath in allNamePictures)
             {
@@ -54,7 +54,7 @@ namespace Viewer
                 NewImage.Height = 150;
                 NewImage.Width = 200;
                 NewImage.MinHeight = 150;
-                NewImage.Margin = new Thickness(2,2,2,2);
+                NewImage.Margin = new Thickness(2, 2, 2, 2);
                 Grid.SetRow(NewImage, Position.ROW);
                 Grid.SetColumn(NewImage, Position.COLUMN);
                 Position.NextPosition(MainGrid);
@@ -68,100 +68,127 @@ namespace Viewer
         }
         private void MainGrid_Drop(object sender, DragEventArgs e)
         {
-            if(e.Data.GetDataPresent(DataFormats.Bitmap))
+            if (!e.Data.GetDataPresent(DataFormats.FileDrop))
             {
-                textBox.Text = "BITMAP";
+                return;
             }
-            if (e.Data.GetDataPresent(DataFormats.Rtf))
+            string[] files = (string[])e.Data.GetData(DataFormats.FileDrop);
+            foreach (string str in files)
             {
-                textBox.Text = "RTF";
+                //сделать проверку попадания одинаковых картинок
+                Image NewImage = new Image();
+                BitmapImage bitImg = new BitmapImage();
+                bitImg.BeginInit();
+                bitImg.UriSource = new Uri(str);
+                bitImg.EndInit();
+                NewImage.Stretch = Stretch.Uniform;
+                NewImage.Source = bitImg;
+                NewImage.Height = 150;
+                NewImage.Width = 200;
+                NewImage.MinHeight = 150;
+                NewImage.Margin = new Thickness(2, 2, 2, 2);
+                Grid.SetRow(NewImage, Position.ROW);
+                Grid.SetColumn(NewImage, Position.COLUMN);
+                Position.NextPosition(MainGrid);
+                allPictures.Add(NewImage);
+                //тут могут быть вопросы...
+                MainGrid.Children.Add(NewImage);
             }
-            if (e.Data.GetDataPresent(DataFormats.Text))
-            {
-                textBox.Text = "TEXT";
-            }
-            if (e.Data.GetDataPresent(DataFormats.CommaSeparatedValue))
-            {
-                textBox.Text = "CommaSeparatedValue";
-            }
-            if (e.Data.GetDataPresent(DataFormats.Dib))
-            {
-                textBox.Text = "Dib";
-            }
-            if (e.Data.GetDataPresent(DataFormats.Dif))
-            {
-                textBox.Text = "Dif";
-            }
-            if(e.Data.GetDataPresent(DataFormats.EnhancedMetafile))
-            {
-                textBox.Text = "EnhancedMetafile";
-            }
-            if (e.Data.GetDataPresent(DataFormats.FileDrop))
-            {
-                FileDrop
-                ()e.Data.GetData(DataFormats.FileDrop);
-                textBox.Text = "FileDrop";
-            }
-            if (e.Data.GetDataPresent(DataFormats.Html))
-            {
-                textBox.Text = "Html";
-            }
-            if (e.Data.GetDataPresent(DataFormats.Locale))
-            {
-                textBox.Text = "Locale";
-            }
-            if (e.Data.GetDataPresent(DataFormats.MetafilePicture))
-            {
-                textBox.Text = "MetafilePicture";
-            }
-            if (e.Data.GetDataPresent(DataFormats.OemText))
-            {
-                textBox.Text = "OemText";
-            }
-            if (e.Data.GetDataPresent(DataFormats.Palette))
-            {
-                textBox.Text = "Palette";
-            }
-            if (e.Data.GetDataPresent(DataFormats.PenData))
-            {
-                textBox.Text = "PenData";
-            }
-            if (e.Data.GetDataPresent(DataFormats.Riff))
-            {
-                textBox.Text = "Riff";
-            }
-            if (e.Data.GetDataPresent(DataFormats.Serializable))
-            {
-                textBox.Text = "Serializable";
-            }
-            if (e.Data.GetDataPresent(DataFormats.StringFormat))
-            {
-                textBox.Text = "StringFormat";
-            }
-            if (e.Data.GetDataPresent(DataFormats.SymbolicLink))
-            {
-                textBox.Text = "SymbolicLink";
-            }
-            if (e.Data.GetDataPresent(DataFormats.Tiff))
-            {
-                textBox.Text = "Tiff";
-            }
-            if (e.Data.GetDataPresent(DataFormats.UnicodeText))
-            {
-                textBox.Text = "UnicodeText";
-            }
-            if (e.Data.GetDataPresent(DataFormats.WaveAudio))
-            {
-                textBox.Text = "WaveAudio";
-            }
-            if (e.Data.GetDataPresent(DataFormats.Xaml))
-            {
-                textBox.Text = "XAML";
-            }
-            if (e.Data.GetDataPresent(DataFormats.XamlPackage))
-            {
-                textBox.Text = "XamlPackage";
-            }
+
+
+
+            //if(e.Data.GetDataPresent(DataFormats.Bitmap))
+            //{
+            //    textBox.Text = "BITMAP";
+            //}
+            //if (e.Data.GetDataPresent(DataFormats.Rtf))
+            //{
+            //    textBox.Text = "RTF";
+            //}
+            //if (e.Data.GetDataPresent(DataFormats.Text))
+            //{
+            //    textBox.Text = "TEXT";
+            //}
+            //if (e.Data.GetDataPresent(DataFormats.CommaSeparatedValue))
+            //{
+            //    textBox.Text = "CommaSeparatedValue";
+            //}
+            //if (e.Data.GetDataPresent(DataFormats.Dib))
+            //{
+            //    textBox.Text = "Dib";
+            //}
+            //if (e.Data.GetDataPresent(DataFormats.Dif))
+            //{
+            //    textBox.Text = "Dif";
+            //}
+            //if(e.Data.GetDataPresent(DataFormats.EnhancedMetafile))
+            //{
+            //    textBox.Text = "EnhancedMetafile";
+            //}
+            //if (e.Data.GetDataPresent(DataFormats.FileDrop))
+            //{
+            //    textBox.Text = "FileDrop";
+            //}
+            //if (e.Data.GetDataPresent(DataFormats.Html))
+            //{
+            //    textBox.Text = "Html";
+            //}
+            //if (e.Data.GetDataPresent(DataFormats.Locale))
+            //{
+            //    textBox.Text = "Locale";
+            //}
+            //if (e.Data.GetDataPresent(DataFormats.MetafilePicture))
+            //{
+            //    textBox.Text = "MetafilePicture";
+            //}
+            //if (e.Data.GetDataPresent(DataFormats.OemText))
+            //{
+            //    textBox.Text = "OemText";
+            //}
+            //if (e.Data.GetDataPresent(DataFormats.Palette))
+            //{
+            //    textBox.Text = "Palette";
+            //}
+            //if (e.Data.GetDataPresent(DataFormats.PenData))
+            //{
+            //    textBox.Text = "PenData";
+            //}
+            //if (e.Data.GetDataPresent(DataFormats.Riff))
+            //{
+            //    textBox.Text = "Riff";
+            //}
+            //if (e.Data.GetDataPresent(DataFormats.Serializable))
+            //{
+            //    textBox.Text = "Serializable";
+            //}
+            //if (e.Data.GetDataPresent(DataFormats.StringFormat))
+            //{
+            //    textBox.Text = "StringFormat";
+            //}
+            //if (e.Data.GetDataPresent(DataFormats.SymbolicLink))
+            //{
+            //    textBox.Text = "SymbolicLink";
+            //}
+            //if (e.Data.GetDataPresent(DataFormats.Tiff))
+            //{
+            //    textBox.Text = "Tiff";
+            //}
+            //if (e.Data.GetDataPresent(DataFormats.UnicodeText))
+            //{
+            //    textBox.Text = "UnicodeText";
+            //}
+            //if (e.Data.GetDataPresent(DataFormats.WaveAudio))
+            //{
+            //    textBox.Text = "WaveAudio";
+            //}
+            //if (e.Data.GetDataPresent(DataFormats.Xaml))
+            //{
+            //    textBox.Text = "XAML";
+            //}
+            //if (e.Data.GetDataPresent(DataFormats.XamlPackage))
+            //{
+            //    textBox.Text = "XamlPackage";
+            //}
             //foreach (string item in e.Data.GetFormats(true))
             //{
             //    textBox.Text += item + "\n";
